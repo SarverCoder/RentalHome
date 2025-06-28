2a729361c6d3f49b26e5ba0a8cfafa84573c2808
Sarvarbek Ibodullayev <sarvargamer8544@gmail.com>
2025-06-28 19:43:32 +0500

    Refactor User class properties for clarity and simplicity
    Removed several properties from the User class, including PasswordHash, Email, PhoneNumber, FullName, Lastname, Firstname, IsActive, Salt, and RefreshToken. Added FirstName and LastName properties, renaming the previous Firstname and Lastname. The removal of IsActive may impact user status management.

--- src/RentalHome.Core/Entities/User.cs
+++ src/RentalHome.Core/Entities/User.cs
@@ -4,13 +4,6 @@ namespace RentalHome.Core.Entities;
 
 public class User : BaseEntity
 {
-    public string PasswordHash { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string? PhoneNumber { get; set; }
-    public string FullName { get; set; } = null!;
-    public string Lastname { get; set; } = null!;
-    public string Firstname { get; set; } = null!;
-    public bool IsActive { get; set; } = true;
-    public string Salt { get; set; }
-    public string? RefreshToken { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
 }
