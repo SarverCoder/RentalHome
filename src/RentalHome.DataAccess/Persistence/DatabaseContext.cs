823b3e76634fefde220ef79a9201d66a4710b24c
Sarvarbek Ibodullayev <sarvargamer8544@gmail.com>
2025-07-02 07:11:09 +0500

    udated

--- src/RentalHome.DataAccess/Persistence/DatabaseContext.cs
+++ src/RentalHome.DataAccess/Persistence/DatabaseContext.cs
@@ -9,11 +9,23 @@ public class DatabaseContext : DbContext
     public DatabaseContext(DbContextOptions options) : base(options)
     { }
 
-    public DbSet<Property> Property { get; set; }
-    public DbSet<User> Users { get; set; }
-    public DbSet<Booking> Bookings { get; set; }
-    public DbSet<Landlord> Landlords { get; set; }
     public DbSet<Amenity> Amenities { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<District> Districts { get; set; }
+    public DbSet<Landlord> Landlords { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    public DbSet<Permission> Permissions { get; set; }
+    public DbSet<Photo> Photos { get; set; }
+    public DbSet<Property> Properties { get; set; }
+    public DbSet<PropertyAmenity> PropertyAmenities { get; set; }
+    public DbSet<PropertyType> PropertyTypes { get; set; }
+    public DbSet<Region> Regions { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<RolePermission> RolePermissions { get; set; }
+    public DbSet<Tenant> Tenants { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
