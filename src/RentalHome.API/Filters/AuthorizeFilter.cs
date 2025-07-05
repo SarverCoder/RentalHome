b07b01962f5e1a078104a2aba61227ee390c3d1c
Sarvarbek Ibodullayev <sarvargamer8544@gmail.com>
2025-07-05 19:16:40 +0500

    Enhance authentication and authorization features
    - Updated `RentalHome.API.csproj` to include new property group and removed folder references.
    - Added `EmailConfiguration` section in `appsettings.json` for SMTP settings.
    - Introduced `Fullname` and `IsVerified` properties in the `User` class.
    - Created custom `AuthorizeAttribute` and `AuthorizeFilter` for permission-based authorization.
    - Added `ApplicationEnumDescriptionAttribute` and `ApplicationPermissionDescriptionAttribute` for enum descriptions.
    - Defined `IUser` interface and implemented `UserAuthModel` for authenticated user data.
    - Created `ApplicationPermissionCode` and `ApplicationPermissionGroupCode` enums for permission management.
    - Defined `IAuthService` interface and implemented `AuthService` for user authentication and permission checks.

---
+++ src/RentalHome.API/Filters/AuthorizeFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RentalHome.Application.Security.AuthEnums;
+using RentalHome.Application.Services;
+
+namespace RentalHome.API.Filters;
+
+public class AuthorizeFilter : IAsyncAuthorizationFilter, IFilterMetadata
+{
+    private readonly ApplicationPermissionCode[] _permissionCodes;
+
+    public AuthorizeFilter(ApplicationPermissionCode[] permissionCodes)
+    {
+        _permissionCodes = permissionCodes;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        if (context.Filters.Any(f => f is AllowAnonymousAttribute))
+        {
+            return;
+        }
+
+        var authService = context.HttpContext.RequestServices.GetService<IAuthService>();
+
+        if (authService == null || !authService.IsAuthenticated)
+        {
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+
+        if (_permissionCodes.Length > 0 && !_permissionCodes.All(p => authService.HasPermission(p.ToString())))
+        {
+            context.Result = new ForbidResult();
+        }
+
+    }
+
+}
