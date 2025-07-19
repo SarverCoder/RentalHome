6fc46fdadba94e58109304714442fd97435e82ea
Sarvarbek Ibodullayev <sarvargamer8544@gmail.com>
2025-07-19 19:49:44 +0500

    Update Minio settings and add image file
    Modified Minio configuration in `appsettings.json` to update the `Endpoint`, `AccessKey`, and `SecretKey`, and added a new `Port` property. Updated `MinioSettings.cs` to include the new `Port` property. Added a new binary image file (`34b3a6b8-bd8e-4d5e-ace8-4ba38c873822.jpg`).

--- src/RentalHome.Application/Common/MinioSettings.cs
+++ src/RentalHome.Application/Common/MinioSettings.cs
@@ -5,6 +5,7 @@ public class MinioSettings
     public string Endpoint { get; set; } = null!;
     public string AccessKey { get; set; } = null!;
     public string SecretKey { get; set; } = null!;
+    public int Port { get; set; }
     public bool UseSsl { get; set; }
     public string? BucketName { get; set; } // Agar HTTPS ishlatayotgan bo'lsangiz true, aks holda false
 }
